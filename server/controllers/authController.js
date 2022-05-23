c11870e6aa97296568c4602f0f24c9fbad194343
Ethan Sclarsky <esclarsky@gmail.com>
2022-05-23 17:12:23 -0400

    startup server files

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,19 @@
-import express from 'express';
+const authController = {}
 
-const authController = {
+authController.signUp = ((req, res, next)=>{
+  const {username=null, password=null} = req.body;
+  const newUser = [username, password];
+  const query = `INSERT INTO users (username, password)
+  VALUES ($1, $2)
+  ;`
 
-}
+  db.query(query, newUser)
+  .then(data => {
+    next();
+  })
+  .catch(err => {
+    next(err);
+  });
+})
 
 export default authController;
