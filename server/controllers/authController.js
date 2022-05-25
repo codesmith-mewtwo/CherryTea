7db95d23429159cef4de0cc23f3e7e589de8c3fd
Ethan Sclarsky <esclarsky@gmail.com>
2022-05-25 17:01:51 -0400

    commented for future devs :)

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,11 @@
+// Controller for login and signup middleware
+
 const authController = {}
-const db = require('../models/dbModel')
+const db = require('../models/dbModel') // connection to elephantSQL database
+
+// signUp middleware function - takes username and password from request body
+// adds them to database (will error out if username already exists in db)
+// then places the UUID of the user into res.locals for use in cookieController
 
 authController.signUp = ((req, res, next)=>{
   const {username=null, password=null} = req.body;
@@ -17,6 +23,10 @@ authController.signUp = ((req, res, next)=>{
     next(err);
   });
 })
+
+// logIn middleware function - takes username and password from request body
+// checks for them in database (will error out if username and pw are not exact per database)
+// then places the UUID of the user into res.locals for use in cookieController
 
 authController.logIn = ((req, res, next)=>{
   const {username=null, password=null} = req.body;
--- server/controllers/cookieController.js
+++ server/controllers/cookieController.js
@@ -1,9 +1,11 @@
 const db = require('../models/dbModel');
 const cookieController = {};
 
-/**
-* setSSIDCookie - store the user id in a cookie
-*/
+
+// setUUIDCookie is last in the middleware chain on signup or login,
+// will take the user's UUID after successful login or signup
+// and place it on a cookie for future use 
+
 cookieController.setUUIDCookie = (req, res, next) => {
   // write code here
   const uuid = res.locals.UUID;
