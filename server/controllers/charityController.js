da7a09fcb9d84982c9a0d94daa912c6428abc720
Ethan Sclarsky <esclarsky@gmail.com>
2022-05-25 10:22:46 -0400

    add charity work

--- server/controllers/charityController.js
+++ server/controllers/charityController.js
@@ -6,7 +6,8 @@ const got = require('got')
 charityController.getCharities = async (req,res,next) => {
   // Take in array of charity names, retrieve relevant information from the API, update these charities in the database, then 
   // return them out with details on res.locals
-  // const {uuid} = req.cookies;
+
+  const {uuid} = req.cookies;
 
   console.log(res.locals.charityNames) // Array of charity names to update
 
@@ -49,32 +50,28 @@ charityController.getCharities = async (req,res,next) => {
       }
   })
 
-  // const query = `
-  // SELECT * FROM charities
-  // INNER JOIN users_charities ON users_charities.charity_id = charities._id
-  // WHERE user_id = '${uuid}'
-  // ;`
-  // db.query(query)
-  // .then(data => {
-  //   res.locals.profile = data.rows;
-  //   next();
-  // })
-  // .catch(err => {
-  //   next(err);
-  // });
+  const query = `
+  SELECT * FROM charities
+  INNER JOIN users_charities ON users_charities.charity_id = charities._id
+  WHERE user_id = '${uuid}'
+  ;`
+  db.query(query)
+  .then(data => {
+    res.locals.profile = data.rows;
+    next();
+  })
+  .catch(err => {
+    next(err);
+  });
 };
 
-charityController.addCharity= (req,res,next) => {
+charityController.addCharity= async (req,res,next) => {
   // Take in charity name, add it to database
   const {charityName} = req.body;
+  try {
+    const response = await got('https://api.data.charitynavigator.org/v2/Organizations?app_id=daae8b79&app_key=7638e36419c57be84cfd662e3d089c0e&pageSize=1&search=Equal%20Justice%20Initiative&searchType=NAME_ONLY').json();
+  }
 
-  got('https://api.data.charitynavigator.org/v2/Organizations?app_id=daae8b79&app_key=7638e36419c57be84cfd662e3d089c0e&search=equal%20justice%20initiative', 
-   { json: true }).then(response => {
-    console.log(response.body.url);
-    console.log(response.body.explanation);
-   }).catch(error => {
-    console.log(error.response.body);
-   });
 
 
 
